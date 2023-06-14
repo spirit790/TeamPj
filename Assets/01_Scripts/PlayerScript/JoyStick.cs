55775f700afac1ead63141701514b25c19dce5cf
황원진 <hwj710314@gmail.com>
2023-06-14 15:47:33 +0900

    re/JoyStick
    JoyStick : 피봇 수정, 화면터치시 조이스틱 보이게

--- Assets/01_Scripts/PlayerScript/JoyStick.cs
+++ Assets/01_Scripts/PlayerScript/JoyStick.cs
@@ -4,34 +4,37 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class JoyStick : MonoBehaviour,IPointerDownHandler,IDragHandler,IPointerUpHandler
+public class JoyStick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
+    GameObject joyStick;
     Image imageBackGround;
     Image imageController;
     Vector2 touchPosition;
-
-    private void Awake()
+    Vector2 touchScreenPosition;
+    void Start()
     {
-        imageBackGround = GetComponent<Image>();
-        imageController = transform.GetChild(0).GetComponent<Image>();
+        joyStick = transform.GetChild(0).gameObject;
+        imageBackGround = joyStick.transform.GetChild(0).gameObject.GetComponent<Image>();
+        imageController = imageBackGround.transform.GetChild(0).gameObject.GetComponent<Image>();
     }
-    /// <summary>
-    /// 눌렀을때 확인되는곳
-    /// </summary>
-    /// <param name="eventData"></param>
+
+
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("Begin");
+        touchScreenPosition = Vector2.zero;
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            GetComponent<Image>().rectTransform, eventData.position, eventData.pressEventCamera, out touchScreenPosition))
+        {
+            joyStick.transform.position = touchScreenPosition;
+        }
+        imageBackGround.enabled = true;
+        imageController.enabled = true;
     }
-  /// <summary>
-  /// 메인으로 움직이는 함수
-  /// </summary>
-  /// <param name="eventData"></param>
     public void OnDrag(PointerEventData eventData)
     {
-        touchPosition = Vector2.zero;
-        if(RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            imageBackGround.rectTransform, eventData.position, eventData.pressEventCamera,out touchPosition))
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            imageBackGround.rectTransform, eventData.position, eventData.pressEventCamera, out touchPosition))
         {
             touchPosition.x = (touchPosition.x / imageBackGround.rectTransform.sizeDelta.x);
             touchPosition.y = (touchPosition.y / imageBackGround.rectTransform.sizeDelta.y);
@@ -40,32 +43,30 @@ public class JoyStick : MonoBehaviour,IPointerDownHandler,IDragHandler,IPointerU
 
             touchPosition = (touchPosition.magnitude > 1) ? touchPosition.normalized : touchPosition;
 
+
             imageController.rectTransform.anchoredPosition = new Vector2(
-                touchPosition.x * imageBackGround.rectTransform.sizeDelta.x/2,
-                touchPosition.y * imageBackGround.rectTransform.sizeDelta.y/2);
-            
+                touchPosition.x * imageBackGround.rectTransform.sizeDelta.x / 2,
+                touchPosition.y * imageBackGround.rectTransform.sizeDelta.y / 2);
         }
 
-        Debug.Log("Drag");
+        //Debug.Log($"{touchPosition.x},{touchPosition.y}");
     }
-    /// <summary>
-    /// 터치를 떼었을때 포지션을 0으로 바꿔주는함수
-    /// </summary>
-    /// <param name="eventData"></param>
     public void OnPointerUp(PointerEventData eventData)
     {
         imageController.rectTransform.anchoredPosition = new Vector2(0, 0);
         touchPosition = Vector2.zero;
         Debug.Log("End");
-    }
 
+        imageBackGround.enabled = false;
+        imageController.enabled = false;
+    }
     public float Horizontal()
     {
-        return touchPosition.x;
+        return touchPosition.normalized.x;
     }
     public float Vertical()
     {
-        return touchPosition.y;
+        return touchPosition.normalized.y;
     }
 
 }
