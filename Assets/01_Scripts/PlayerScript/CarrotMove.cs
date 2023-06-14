4611161ff89d207c39b1cc6f30a29b5d56136793
spirit790 <rkdqhdrnt@naver.com>
2023-06-14 14:12:09 +0900

    add/DashBtn,PlayerMove
    세부수정

--- Assets/01_Scripts/PlayerScript/CarrotMove.cs
+++ Assets/01_Scripts/PlayerScript/CarrotMove.cs
@@ -27,7 +27,7 @@ public class CarrotMove : MonoBehaviour
         
     }
     /// <summary>
-    /// 버튼을 눌렀을시 콜라이더 z축으로 atkLength만큼 ray를 발사하여 공격판정을합니다.
+    /// 버튼을 눌렀을시 콜라이더 z축으로 atkLength만큼 ray를 발사하여 atkLength만큼 공격판정을합니다.
     /// </summary>
     public void OnClickAtk()
     {
--- Assets/01_Scripts/PlayerScript/Player.cs
+++ Assets/01_Scripts/PlayerScript/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
@@ -10,21 +11,29 @@ public class Player : MonoBehaviour
     public Animator playerAnim;
 
     public float moveSpeed;
+    public float normalSpeed;
     public float dashSpeed;
     public float atkSpeed;
-    public Button dashBtn;
+    public GameObject dashBtn;
+    bool check;
 
     public float curTime;
     public float coolTime;
     Transform tr;
+    Rigidbody rBody;
 
-
+    private void Awake()
+    {
+        tr = GetComponent<Transform>();
+        rBody = GetComponent<Rigidbody>();
+        joyStick = GameObject.FindGameObjectWithTag("PlayerCanvas").GetComponentInChildren<JoyStick>();
+        dashBtn = GameObject.FindGameObjectWithTag("DashBtn");
+        dashBtn.GetComponent<DashBtn>();
+    }
 
     void Start()
     {
-        joyStick = GameObject.FindGameObjectWithTag("PlayerCanvas").GetComponentInChildren<JoyStick>();
-        tr = GetComponent<Transform>();
-        dashBtn = GameObject.FindGameObjectWithTag("DashBtn").GetComponent<Button>();
+        
     }
 
     void Update()
@@ -37,23 +46,34 @@ public class Player : MonoBehaviour
     {
         float h = joyStick.Horizontal();
         float v = joyStick.Vertical();
+        float fall = rBody.velocity.y;
 
         if (h != 0 || v != 0)
         {
-            Vector3 moveDiection = new Vector3(h, 0, v).normalized;
-            tr.position += moveDiection * moveSpeed * Time.deltaTime;
+            
+
+            //velocity로 이동
+            rBody.velocity = new Vector3(h * moveSpeed, fall, v * moveSpeed);
+            //transform.position으로 이동
+            //Vector3 moveDiection = new Vector3(h, 0, v).normalized;
+            //tr.position += moveDiection * moveSpeed * Time.deltaTime;
         }
     }
     public void PlayerKeyBordMove()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-
-        Vector3 moveDiection = new Vector3(h, 0, v).normalized;
-        tr.position += moveDiection * moveSpeed * Time.deltaTime;
-    }
-    public void OnclickDash()
-    {
-        Debug.Log("대시버튼");
+        float fall = rBody.velocity.y;
+        if (check)
+        {
+            Debug.Log("~!!!");
+            rBody.velocity = new Vector3(h * moveSpeed, fall, v * moveSpeed);
+        }
+        else if (!check)
+        {
+            rBody.velocity = new Vector3(h * moveSpeed, fall, v * moveSpeed);
+        }
+        //Vector3 moveDiection = new Vector3(h, 0, v).normalized;
+        //tr.position += moveDiection * moveSpeed * Time.deltaTime;
     }
 }
