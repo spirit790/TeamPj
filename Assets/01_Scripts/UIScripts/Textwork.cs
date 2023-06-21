f790e9c6e8811ef3e48a9f177e7089c46cd9262e
geneleeverse <leewj2975@gmail.com>
2023-06-21 10:35:29 +0900

    re/Textwork
    모션 수정

--- Assets/01_Scripts/UIScripts/Textwork.cs
+++ Assets/01_Scripts/UIScripts/Textwork.cs
@@ -22,12 +22,21 @@ public class Textwork : MonoBehaviour
         }
     }
 
-    public TMP_FontAsset[] fonts;
+    public TMP_FontAsset font;
     public Canvas mainCanvas;
     [Range(1, 2000)]
     public int defaultFontSize = 1200;
     private Vector3 originPos;
+    [Range(0.3f, 1f)]
+    public float linearFadeInTime = 0.4f;
+    [Range(0.1f, 0.3f)]
+    public float linearFadeOutTime = 0.2f;
+
+    public Shader fontShader;
+
+    [Header("Test")]
     public int testKillCnt = 7;
+    public Button sqBtn;
     
     private int TestKillCnt
     {
@@ -44,9 +53,11 @@ public class Textwork : MonoBehaviour
 
     void Start()
     {
-        originPos = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2);
 
         DOTween.Init();
+
+        sqBtn.onClick.AddListener(OnClickCountDown);
+        sqBtn.onClick.AddListener(OnClickSceneCall);
     }
 
     public void OnClickCountDown()
@@ -76,10 +87,13 @@ public class Textwork : MonoBehaviour
     /// <param name="alpha"></param>
     public TextMeshProUGUI InitText(Color color, int fontSize = 1200, float alpha = 1f, float posDivideX = 1, float posDivideY = 1)
     {
+        originPos = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2);
+
         GameObject txtObj = new GameObject();
         txtObj.name = "textworks";
         TextMeshProUGUI txt = txtObj.AddComponent<TextMeshProUGUI>();
         txt.gameObject.transform.SetParent(mainCanvas.transform);
+        //txt.material.shader = fontShader;
 
         if (posDivideX <= 0 || posDivideY <= 0) posDivideX = posDivideY = 1;
         txt.rectTransform.position = new Vector3(originPos.x / posDivideX, originPos.y / posDivideY);
@@ -96,12 +110,80 @@ public class Textwork : MonoBehaviour
         return txt;
     }
 
+    /// <summary>
+    /// 4방향 텍스트워크
+    /// </summary>
+    /// <param name="txt">움직일 텍스트(TMP)</param>
+    /// <param name="textString"></param>
+    /// <param name="dir">1 = rightward, 2 = leftward, 3 = upward, 4 = downward</param>
+    /// <param name="fontSizeRatio"></param>
+    /// <param name="posOffset"></param>
+    /// <param name="fadeIn"></param>
+    /// <param name="fadeOut"></param>
+    /// <returns></returns>
+    public IEnumerator LinearTextwork(TextMeshProUGUI txt, string textString, int dir = 1, int fontSizeRatio = 3, float posOffset = 0f, float fadeIn = 0.4f, float fadeOut = 0.2f)
+    {
+        if (dir < 1 || dir > 4) yield break;
+       
+        txt.text = textString;
+        //txt.font = fonts[0];
+        if(fontSizeRatio > 0) txt.fontSize = (int)txt.fontSize / fontSizeRatio;
+
+        switch (dir)
+        {
+            case 1:
+                txt.rectTransform.position = new Vector3(this.originPos.x / 3 + posOffset, this.originPos.y, 0);
+
+                txt.rectTransform.DOMoveX(this.originPos.x, fadeIn);
+
+                yield return new WaitForSeconds(fadeIn);
+
+                txt.rectTransform.DOMoveX(this.originPos.x * 4 / 3 + posOffset, fadeOut);
+
+                break;
+            case 2:
+                txt.rectTransform.position = new Vector3(this.originPos.x * 4 / 3 + posOffset, this.originPos.y, 0);
+
+                txt.rectTransform.DOMoveX(this.originPos.x, fadeIn);
+
+                yield return new WaitForSeconds(fadeIn);
+
+                txt.rectTransform.DOMoveX(this.originPos.x / 3 + posOffset, fadeOut);
+
+                break;
+            case 3:
+                txt.rectTransform.position = new Vector3(this.originPos.x, this.originPos.y / 3 + posOffset, 0);
+
+                txt.rectTransform.DOMoveY(this.originPos.y, fadeIn);
+
+                yield return new WaitForSeconds(fadeIn);
+
+                txt.rectTransform.DOMoveY(this.originPos.y * 4 / 3 + posOffset, fadeOut);
+
+                break;
+            case 4:
+                txt.rectTransform.position = new Vector3(this.originPos.x, this.originPos.y * 4 / 3 + posOffset, 0);
+
+                txt.rectTransform.DOMoveY(this.originPos.y, fadeIn);
+
+                yield return new WaitForSeconds(fadeIn);
+
+                txt.rectTransform.DOMoveY(this.originPos.y / 3 + posOffset, fadeOut);
+
+                break;
+            default:
+                break;
+        }
+
+        txt.DOFade(0, fadeOut);
+        yield return new WaitForSeconds(fadeOut / 1.3f);
+    }
 
     /// <summary>
     /// 게임 시작시 신 콜
     /// 예시 텍스트는 추후 데이터화
     /// </summary>
-    public IEnumerator SceneCall(int mode, int posOffset = 300, float fadeIn = 1f, float fadeOut = 0.2f)
+    public IEnumerator SceneCall(int mode, int posOffset = 300, float fadeIn = 0.9f, float fadeOut = 0.1f)
     {
         if (mode < 1 || mode > 3) yield break;
 
@@ -115,14 +197,14 @@ public class Textwork : MonoBehaviour
                 txts[0].DOFade(0.5f, fadeIn);
                 txts[0].transform.DOMoveX(txts[0].rectTransform.position.x - posOffset / 1.5f, fadeIn);
 
-                yield return new WaitForSeconds(fadeIn/2);
+                yield return new WaitForSeconds(fadeIn/3);
 
                 txts[1] = InitText(Color.white, defaultFontSize / 4, 0.0f, 0.75f, 1);
                 txts[1].text = "THE LAST";
                 txts[1].DOFade(0.5f, fadeIn);
                 txts[1].transform.DOMoveX(txts[1].rectTransform.position.x + posOffset/3f, fadeIn);
 
-                yield return new WaitForSeconds(fadeIn/2);
+                yield return new WaitForSeconds(fadeIn/3);
 
                 txts[2] = InitText(Color.white, defaultFontSize / 4, 0.0f, 1.3f, 2f);
                 txts[2].text = "STANDING";
@@ -147,9 +229,9 @@ public class Textwork : MonoBehaviour
 
         yield return new WaitForSeconds(fadeIn);
 
-        txts[0].transform.DOMoveY(2f * (txts[0].rectTransform.position.x + posOffset), fadeOut);
+        txts[0].transform.DOMoveX(-2f * (txts[0].rectTransform.position.x + posOffset), fadeOut);
         txts[1].transform.DOMoveX(2f * (txts[1].rectTransform.position.x + posOffset), fadeOut);
-        txts[2].transform.DOMoveY(-2f*(txts[2].rectTransform.position.x - posOffset), fadeOut);
+        txts[2].transform.DOMoveX(-2f*(txts[2].rectTransform.position.x - posOffset), fadeOut);
         txts[0].DOFade(0f, fadeOut);
         txts[1].DOFade(0f, fadeOut);
         txts[2].DOFade(0f, fadeOut);
@@ -169,7 +251,7 @@ public class Textwork : MonoBehaviour
     /// <param name="count"></param>
     /// <param name="delay"></param>
     /// <returns></returns>
-    public IEnumerator StartCountDown(int count = 3, int delay = 1, int posOffset = 300, string textString = "START!", float fadeIn = 0.6f, float fadeOut = 0.2f)
+    public IEnumerator StartCountDown(int count = 3, int delay = 1, int posOffset = 300, float fadeIn = 0.6f, float fadeOut = 0.2f)
     {
         TextMeshProUGUI txt = InitText(Color.white, defaultFontSize, 0.5f);
 
@@ -180,23 +262,13 @@ public class Textwork : MonoBehaviour
         for (int i = 0; i <= count + 1; i++)
         {
             txt.text = (--count).ToString();
-            
+
             yield return new WaitForSeconds(delay);
         }
 
-        txt.text = textString;
-        //txt.font = fonts[0];
-        txt.fontSize = (int)txt.fontSize / 3;
+        //txt.DOCounter(count, 1, delay * count);
 
-        txt.rectTransform.position = new Vector3(this.originPos.x - posOffset, this.originPos.y, 0);
-
-        txt.rectTransform.DOMoveX(this.originPos.x + posOffset / 3, fadeIn);
-
-        yield return new WaitForSeconds(fadeIn);
-
-        txt.rectTransform.DOMoveX(txt.rectTransform.position.x + 3f * posOffset, fadeOut);
-        txt.DOFade(0, fadeOut);
-        yield return new WaitForSeconds(fadeOut);
+        yield return StartCoroutine(LinearTextwork(txt, "START!", 1));
 
         Destroy(txt.gameObject);
     }
@@ -207,7 +279,7 @@ public class Textwork : MonoBehaviour
     /// </summary>
     /// <param name="remainsNum"></param>
     /// <returns></returns>
-    public IEnumerator RemainsCountText(int remainsNum, int posOffset = 300, float fadeIn = 0.6f, float fadeOut = 0.2f)
+    public IEnumerator RemainsCountText(int remainsNum, float fadeIn = 0.6f, float fadeOut = 0.2f)
     {
         if (remainsNum <= 0) yield break;
 
@@ -218,47 +290,8 @@ public class Textwork : MonoBehaviour
         txt.DOFade(0.5f, fadeIn);
 
         int rnd = Random.Range(1, 5);
-        switch (rnd)
-        {
-            case 1:
-                txt.rectTransform.position = new Vector3(this.originPos.x - 2 * posOffset, this.originPos.y, 0);
 
-                txt.rectTransform.DOMoveX(this.originPos.x + posOffset / 3, fadeIn);
-
-                yield return new WaitForSeconds(fadeIn);
-
-                txt.rectTransform.DOMoveX(txt.rectTransform.position.x + 3f * posOffset, fadeOut);
-                break;
-            case 2:
-                txt.rectTransform.position = new Vector3(this.originPos.x + 2 * posOffset, this.originPos.y, 0);
-
-                txt.rectTransform.DOMoveX(this.originPos.x - posOffset / 3, fadeIn);
-
-                yield return new WaitForSeconds(fadeIn);
-
-                txt.rectTransform.DOMoveX(txt.rectTransform.position.x - 3f * posOffset, fadeOut);
-                break;
-            case 3:
-                txt.rectTransform.position = new Vector3(this.originPos.x, this.originPos.y - 2 * posOffset, 0);
-
-                txt.rectTransform.DOMoveY(this.originPos.y, fadeIn);
-
-                yield return new WaitForSeconds(fadeIn);
-
-                txt.rectTransform.DOMoveY(txt.rectTransform.position.y + 3f * posOffset, fadeOut);
-                break;
-            case 4:
-                txt.rectTransform.position = new Vector3(this.originPos.x, this.originPos.y + 2 * posOffset, 0);
-
-                txt.rectTransform.DOMoveY(this.originPos.y, fadeIn);
-
-                yield return new WaitForSeconds(fadeIn);
-
-                txt.rectTransform.DOMoveY(txt.rectTransform.position.y - 3f * posOffset, fadeOut);
-                break;
-            default:
-                break;
-        }
+        yield return StartCoroutine(LinearTextwork(txt, remainsNum.ToString(), rnd, 1));
 
         txt.DOFade(0, fadeOut);
         yield return new WaitForSeconds(fadeOut);
