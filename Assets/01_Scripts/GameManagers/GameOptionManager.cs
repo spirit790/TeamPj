3b733592f46066ecf21ad50fe4888b60ab0ebe07
spirit790 <rkdqhdrnt@naver.com>
2023-06-23 09:33:29 +0900

    add/GameOptionManager
    게임옵션매니저 싱글톤으로만들예정

---
+++ Assets/01_Scripts/GameManagers/GameOptionManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+
+public class GameOptionManager : MonoBehaviour
+{
+
+
+
+    //현지화 변수와 함수
+    #region Localization
+    bool isChanging = false;
+
+    public void ChangeLocale(int index)
+    {
+        if (isChanging)
+            return;
+
+        StartCoroutine(LocaleChange(index));
+    }
+
+    IEnumerator LocaleChange(int index)
+    {
+        isChanging = true;
+
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
+
+        isChanging = false;
+    }
+    #endregion
+
+    //사운드 옵션
+    #region SoundOption
+
+    #endregion
+
+
+}
---
+++ Assets/01_Scripts/GameManagers/GoogleManager.cs
@@ -0,0 +1,277 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GooglePlayGames;
+using Firebase.Firestore;
+using Firebase.Extensions;
+
+public class GoogleManager : MonoBehaviour
+{
+    public static GoogleManager instance;
+    public static GoogleManager Instance
+    {
+        get { return instance; }
+    }
+    [Header("UI")]
+    public Text logText;
+    public InputField nickNameInput;
+    public GameObject signUpPanel;
+    public GameObject updatePanel;
+
+    private bool isGoogleLoginSuccess = false;
+    public bool IsGoogleLoginSuccess
+    {
+        get { return isGoogleLoginSuccess; }
+        set
+        {
+            if (value == true)
+            {
+                // 회원가입 안되어있다면 가입하도록
+                StartCoroutine(CheckUserInfo(Social.localUser.id));
+            }
+        }
+    }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+#if UNITY_ANDROID
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+#endif
+        Login();
+    }
+
+
+    public void Login()
+    {
+        Social.localUser.Authenticate((bool success) =>
+        {
+            if (success)
+            {
+                logText.text = Social.localUser.id + "\n" + Social.localUser.userName;
+                IsGoogleLoginSuccess = true;
+            }
+            else logText.text = "login failed";
+        });
+    }
+
+    public void LogOut()
+    {
+#if UNITY_ANDROID
+        ((PlayGamesPlatform)Social.Active).SignOut();
+        logText.text = "logout";
+#endif
+    }
+
+    IEnumerator CheckUserInfo(string userId)
+    {
+        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+        DocumentReference docRef = db.Collection("users").Document(userId);
+        yield return docRef.GetSnapshotAsync().ContinueWithOnMainThread(task =>
+        {
+            DocumentSnapshot snapShot = task.Result;
+            if (snapShot.Exists)
+            {
+                // 게임시작 씬전환 등
+                updatePanel.SetActive(true);
+            }
+            else
+            {
+                // 회원가입창
+                signUpPanel.SetActive(true);
+            }
+        });
+    }
+
+    public void OnCreateUser()
+    {
+        StartCoroutine(CreateUserInfo(nickNameInput.text));
+    }
+
+    IEnumerator CreateUserInfo(string nickName)
+    {
+        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+        DocumentReference docRef = db.Collection("users").Document(Social.localUser.id);
+        Dictionary<string, object> user = new Dictionary<string, object>
+        {
+            { "UserName", Social.localUser.userName },
+            { "NickName", nickName },
+            { "Kills", 0 },
+            { "Wins", 0 },
+            { "Plays", 0},
+            { "PlayerKills", 0 },
+            { "AIKills", 0 },
+            { "Death", 0 },
+            { "isAdsFree", false },
+            { "SignUpDate", FieldValue.ServerTimestamp },
+            { "LoginDate", FieldValue.ServerTimestamp }
+        };
+
+        yield return docRef.SetAsync(user).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCompleted)
+            {
+                Debug.Log("가입 완료");
+                signUpPanel.SetActive(false);
+                updatePanel.SetActive(true);
+            }
+            else
+            {
+                // 에러 처리
+            }
+        });
+    }
+
+    
+    public void OnUpdateUserGameData()
+    {
+        int wins = 0;
+        int plays = 0;
+        int playerKills = 0;
+        int aiKills = 0;
+        int death =0;
+
+
+        foreach (KeyValuePair<string, object> pair in GameManager.Instance.userInfo)
+        {
+            switch (pair.Key)
+            {
+                case "Wins":
+                    Debug.Log($"{pair.Key}: {pair.Value}");
+                    wins = int.Parse(pair.Value.ToString());
+                    if (GameManager.Instance.isWin)
+                    {
+                        wins += 1;
+                        Debug.Log(wins);
+                    }
+                    break;
+                case "Plays":
+                    Debug.Log($"{pair.Key}: {pair.Value}");
+                    plays = int.Parse(pair.Value.ToString());
+                    plays++;
+                    break;
+                case "PlayerKills":
+                    Debug.Log($"{pair.Key}: {pair.Value}");
+                    playerKills = int.Parse(pair.Value.ToString());
+                    playerKills += GameManager.Instance.playerKills;
+                    Debug.Log($"PlayerKills: {playerKills}");
+                    break;
+                case "AIKills":
+                    Debug.Log($"{pair.Key}: {pair.Value}");
+                    aiKills = int.Parse(pair.Value.ToString());
+                    aiKills += GameManager.Instance.aiKills;
+                    Debug.Log($"AIKills: {aiKills}");
+                    break;
+                case "Death":
+                    Debug.Log($"{pair.Key}: {pair.Value}");
+                    death = int.Parse(pair.Value.ToString());
+                    death += GameManager.Instance.death;
+                    break;
+                default:
+                    break;
+            }
+        }
+        StartCoroutine(UpdateUserGameData(wins, plays, playerKills, aiKills, death));
+    }
+    IEnumerator UpdateUserGameData(int wins, int plays, int playerKills, int aiKills, int death)
+    {
+        int kills = playerKills + aiKills;
+        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+        DocumentReference docRef = db.Collection("users").Document(Social.localUser.id);
+        Dictionary<string, object> gameData = new Dictionary<string, object>
+        {
+            { "Kills", kills },
+            { "Wins", wins },
+            { "Plays", plays },
+            { "PlayerKills", playerKills },
+            { "AIKills", aiKills },
+            { "Death", death },
+        };
+        yield return docRef.UpdateAsync(gameData).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCompleted)
+            {
+                Debug.Log("Update User GameData");
+            }
+            else
+            {
+                // 에러 처리
+            }
+        });
+
+    }
+
+
+    IEnumerator UpdateUserLoginDate()
+    {
+        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+        DocumentReference docRef = db.Collection("users").Document(Social.localUser.id);
+        Dictionary<string, object> loginDate = new Dictionary<string, object>
+        {
+            { "LoginDate", FieldValue.ServerTimestamp }
+        };
+        yield return docRef.UpdateAsync(loginDate).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCompleted)
+            {
+                Debug.Log("Update User LoginDate");
+            }
+            else
+            {
+                // 에러 처리
+            }
+        });
+
+    }
+
+    IEnumerator UpdateUserLogoutDate()
+    {
+        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+        DocumentReference docRef = db.Collection("users").Document(Social.localUser.id);
+        Dictionary<string, object> logoutDate = new Dictionary<string, object>
+        {
+            { "LoginDate", FieldValue.ServerTimestamp }
+        };
+        yield return docRef.UpdateAsync(logoutDate).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCompleted)
+            {
+                Debug.Log("Update User LogoutDate");
+            }
+            else
+            {
+                // 에러 처리
+            }
+        });
+    }
+
+    public void OnGetUserInfo()
+    {
+        StartCoroutine(GetUserInfo());
+    }
+    IEnumerator GetUserInfo()
+    {
+        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+        DocumentReference docRef = db.Collection("users").Document(Social.localUser.id);
+        yield return docRef.GetSnapshotAsync().ContinueWithOnMainThread(task =>
+        {
+            DocumentSnapshot snapshot = task.Result;
+            if (snapshot.Exists)
+            {
+                GameManager.Instance.userInfo = snapshot.ToDictionary();
+            }
+        });
+    }
+}
