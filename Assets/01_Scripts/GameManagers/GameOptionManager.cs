e2c3cec87969876ae5d7ed5e637c217d06b7d48a
spirit790 <rkdqhdrnt@naver.com>
2023-06-26 18:22:32 +0900

    re/GameOptionManager
    sound준비끝

--- Assets/01_Scripts/GameManagers/GameOptionManager.cs
+++ Assets/01_Scripts/GameManagers/GameOptionManager.cs
@@ -7,48 +7,55 @@ using UnityEngine.Audio;
 
 public class GameOptionManager : MonoBehaviour
 {
-    public static GameOptionManager instance;
+    static GameOptionManager instance = null;
 
-    [SerializeField]
-    GameObject option;
-
-    
+    public static GameOptionManager Instance()
+    {
+        return instance;
+    }
+    Slider bgmSlider;
+    Slider sfxSlider;
+    GameObject optionPanel;
     AudioSource bgmPlayer;
     AudioSource sfxPlayer;
-    [Header("Audio List")]
-    public AudioClip[] audioClips;
-    public AudioMixer audioMixer;
+    public AudioClip[] bgmClips;
+    public AudioClip[] sfxClips;
 
-    public Slider bgmSlider;
-    public Slider sfxSlider;
-        
+    //public AudioMixer audioMixer;
     private void Awake()
     {
-        instance = this;
+        if(instance ==null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
         bgmPlayer = GameObject.FindWithTag("BgmPlayer").GetComponent<AudioSource>();
         sfxPlayer = GameObject.FindWithTag("SfxPlayer").GetComponent<AudioSource>();
 
-        
+        //PlayerCanvas하위 오브젝트들의 순서에 따라 찾게 해놨습니다.
+        optionPanel = GameObject.FindWithTag("PlayerCanvas").transform.GetChild(0).gameObject;
+        bgmSlider = optionPanel.transform.GetChild(0).GetComponent<Slider>();
+        sfxSlider = optionPanel.transform.GetChild(1).GetComponent<Slider>();
+        bgmSlider.onValueChanged.AddListener(ChangeBgmVol);
+        sfxSlider.onValueChanged.AddListener(ChangeSfxVol);
     }
-
-    private void Start()
-    {
-
-    }
-
-
     private void Update()
     {
         if (Input.GetKey(KeyCode.Escape))
         {
-            option.SetActive(true);
+            optionPanel.SetActive(true);
         }
 #if UNITY_ANDROID
         if (Application.platform == RuntimePlatform.Android)
         {
             if (Input.GetKey(KeyCode.Escape))
             {
-                option.SetActive(true);
+                optionPanel.SetActive(true);
             }
         }
 #endif
@@ -80,8 +87,33 @@ public class GameOptionManager : MonoBehaviour
 
     //사운드 옵션
     #region SoundOption
+    /// <summary>
+    /// case추가하시고 소리가 나야하는 스크립트 위치에 가져오셔서 PlayBgmSound("???")로 쓰면됩니다.
+    /// </summary>
+    /// <param name="type"></param>
+    public void PlayBgmSound(string type)
+    {
+        int index = 0;
 
-    public void PlaySound(string type)
+        switch (type)
+        {
+            case "Intro": index = 0; break;
+            case "Lobby": index = 1; break;
+            case "InGame1": index = 2; break;
+            case "InGame2": index = 3; break;
+            case "InGame3": index = 4; break;
+            case "InGame4": index = 5; break;
+            case "Result": index = 6; break;
+        }
+        bgmPlayer.clip = bgmClips[index];
+        bgmPlayer.loop = true;
+        bgmPlayer.Play();        
+    }
+    /// <summary>
+    /// case추가하시고 소리가 나야하는 스크립트 위치에 가져오셔서 PlaySfxSound("???")로 쓰면됩니다.
+    /// </summary>
+    /// <param name="type"></param>
+    public void PlaySfxSound(string type)
     {
         int index = 0;
 
@@ -93,29 +125,27 @@ public class GameOptionManager : MonoBehaviour
             case "Close": index = 3; break;
         }
 
-        sfxPlayer.clip = audioClips[index];
+        sfxPlayer.clip = sfxClips[index];
         sfxPlayer.Play();
     }
 
-    void OnMouseDown()
+    void ChangeBgmVol(float bgmVol)
     {
-        instance.PlaySound("Touch");
-        instance.PlaySound("Attack");
-        instance.PlaySound("Dash");
-        instance.PlaySound("Close");
+        bgmPlayer.volume = bgmVol;
     }
-
-
+    void ChangeSfxVol(float sfxVol)
+    {
+        sfxPlayer.volume = sfxVol;
+    }
     #endregion
 
     public void CloseOptionPanelBtn()
     {
-        option.SetActive(false);
+        optionPanel.SetActive(false);
     }
 
     public void AppQuitBtn()
     {
         Application.Quit();
     }
-
 }
