7b5218a1a74b2bc6560da927e16ee3474451d984
geneleeverse <leewj2975@gmail.com>
2023-06-26 17:40:02 +0900

    add/modelTest
    모델링 넣어서 애니메이션 및 환경 테스트

--- Assets/01_Scripts/EnvScripts/PlayerOutline_test.cs
+++ Assets/01_Scripts/EnvScripts/PlayerOutline_test.cs
@@ -7,14 +7,16 @@ using UnityEngine.Rendering.Universal;
 [System.Serializable]
 public class PlayerOutline_test : MonoBehaviour
 {
+    public GameObject outlineModel;
     public Material outlineMat;
     public float outLineScale;
     public Color outlineColor;
+    public float offset;
     private Renderer outlineRenderer;
 
     void Start()
     {
-        outlineRenderer = CreateOutline(outlineMat, outLineScale, outlineColor);
+        outlineRenderer = CreateOutline(outlineModel, outlineMat, outLineScale, outlineColor, offset);
     }
 
     void Update()
@@ -29,22 +31,25 @@ public class PlayerOutline_test : MonoBehaviour
         }
     }
 
-    public Renderer CreateOutline(Material outlineMat, float scaleFactor, Color color)
+    public Renderer CreateOutline(GameObject model, Material outlineMat, float scaleFactor, Color color, float offset)
     {
-        GameObject outlineObject = Instantiate(gameObject, transform.position, transform.rotation, transform);
+        GameObject outlineObject = Instantiate(model, model.transform.position, model.transform.rotation, model.transform);
         outlineObject.name = "Outline";
         Renderer rend = outlineObject.GetComponent<Renderer>();
 
         rend.material = outlineMat;
         rend.material.SetColor("_OutlineColor", color);
         rend.material.SetFloat("_Scale", scaleFactor);
+        rend.material.SetFloat("_Offset", offset);
         rend.shadowCastingMode = ShadowCastingMode.Off;
 
         //테스트용
         //모델링에 있는 상호작용 컴포넌트 disabled
-        outlineObject.GetComponent<PlayerOutline_test>().enabled = false;
+        //outlineObject.GetComponent<PlayerOutline_test>().enabled = false;
 
         rend.enabled = false;
+
+        Debug.Log("render2");
 
         return rend;
     }
