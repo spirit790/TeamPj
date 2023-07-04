82e230eb536f6c73aa69d68593204d1ce9dc717c
geneleeverse <leewj2975@gmail.com>
2023-07-04 16:09:46 +0900

    re/merge test

--- Assets/01_Scripts/AnimationScripts/ActorsPopIn.cs
+++ Assets/01_Scripts/AnimationScripts/ActorsPopIn.cs
@@ -5,12 +5,24 @@ using DG.Tweening;
 
 public class ActorsPopIn : MonoBehaviour
 {
+
+    public float rndPos = 1;
+    public float rndTime = 1;
+    public float waitTime = 1;
+
     void Start()
     {
         DOTween.Init();
 
-        Sequence seq = DOTween.Sequence();
+        rndPos = Random.Range(2, 5) * 0.1f;
+        rndTime = Random.Range(2f, 5) * 0.1f;
+        waitTime = Random.Range(1, 5);
 
+        Sequence seq = DOTween.Sequence();
+        seq.Append(transform.DOMoveY(transform.position.y + rndPos, rndTime))
+            .Append(transform.DOMoveY(transform.position.y, rndTime).OnComplete<Tween>(() => {rndPos = Random.Range(0.2f, 0.5f); rndTime = Random.Range(0.2f, 0.5f); waitTime = Random.Range(1, 5); }))
+             .Append(transform.DOMoveY(transform.position.y, waitTime))
+              .SetLoops(-1);
 
     }
 
