ec3e446fe61de9b352aefddc435142249ee2fe2e
geneleeverse <127931593+geneleeverse@users.noreply.github.com>
2023-07-21 00:35:15 +0900

    re/refac
    시야 시스템 리팩토링 중

--- Assets/01_Scripts/EnvScripts/SilhouetteRadar.cs
+++ Assets/01_Scripts/EnvScripts/SilhouetteRadar.cs
@@ -9,75 +9,65 @@ public class SilhouetteRadar : MonoBehaviour
     private Transform playerTr;
     public int radarMaskNum = 6;
     private int layerMask;
-    private float curTime;
     public float radarTime = 2f;
     public float radarRange = 5f;
 
-    void Start()
+    private Dictionary<Transform, SkinnedMeshRenderer[]> actorRenderers;
+
+    IEnumerator Start()
     {
         DOTween.Init();
         layerMask = 1 << radarMaskNum;
+
+
+        yield return null;
+
+        actorRenderers = GameObject.Find("Vision").GetComponent<MeshVisionGen_Updated>().actorRenderers;
+
+        StartCoroutine(InvisibleRadar());
     }
 
-    void Update()
-    {
-        curTime += Time.deltaTime;
 
-        if(curTime >= radarTime)
-        {
-            InvisibleRadar();
-            curTime = 0;
-        }
-    }
-
-    private void InvisibleRadar()
+    IEnumerator InvisibleRadar()
     {
-        if(playerTr != null)
+        while (playerTr != null)
         {
+
             Debug.DrawLine(playerTr.position, playerTr.position + Vector3.right * radarRange, Color.red, 2f, true);
             var actors = Physics.OverlapSphere(playerTr.position, radarRange, layerMask);
 
             foreach (var actor in actors)
             {
-                if (actor.gameObject.CompareTag("Player"))
-                {
-                    if (actor.transform.parent.gameObject.GetPhotonView().IsMine) 
-                        continue;
-                    StartCoroutine(RadarFade(actor.transform, radarTime));
-                }                                       
-                if (actor.gameObject.CompareTag("AI"))
-                {                   
-                    StartCoroutine(RadarFade(actor.transform, radarTime));
-                }
+                if (actor.transform.parent.gameObject.GetPhotonView().IsMine) continue;
 
+                StartCoroutine(RadarFade(actor.transform, radarTime));
             }
+
+            yield return new WaitForSeconds(radarTime);
         }
     }
 
     IEnumerator RadarFade(Transform actor, float freq)
     {
-        SkinnedMeshRenderer[] actorRenderer = actor.GetComponentsInChildren<SkinnedMeshRenderer>();
-        if (actorRenderer == null) yield break;
+        
+        if (actor == null) yield break;
 
-        float rnd = Random.Range(0.5f, 1.5f);
+        float rnd = Random.Range(5, 15) * 0.1f;
 
         yield return new WaitForSeconds(rnd);
 
-        actorRenderer[0].material.DOFade(1, freq / 2);
-        //actorRenderer[1].enabled = true;
-        actorRenderer[2].material.DOFade(1, freq / 2);
+        actorRenderers[actor][0].material.DOFade(1, freq * 0.5f);
+        actorRenderers[actor][2].material.DOFade(1, freq * 0.5f);
 
         //Debug.Log(actor.name + "¹à¾ÆÁø´Ù~");
 
-        yield return new WaitForSeconds(freq / 2);
+        yield return new WaitForSeconds(freq * 0.5f);
 
-        actorRenderer[0].material.DOFade(0, freq / 2);
-        //actorRenderer[1].enabled = false;
-        actorRenderer[2].material.DOFade(0, freq / 2);
-        //weaponRenderer.material.DOFade(0, freq / 2);
+        actorRenderers[actor][0].material.DOFade(0, freq * 0.5f);
+        actorRenderers[actor][2].material.DOFade(0, freq * 0.5f);
         //Debug.Log(actor.name + "Èå·ÁÁø´Ù~");
 
-        yield return new WaitForSeconds(radarTime / 2);
+        yield return new WaitForSeconds(radarTime * 0.5f);
     }
 
     public void SetRadarCenter(Transform target)
