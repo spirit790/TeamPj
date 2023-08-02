89557b97286967fffc6c4305b15f219128e2efd0
geneleeverse <127931593+geneleeverse@users.noreply.github.com>
2023-08-02 15:08:23 +0900

    re

--- Assets/01_Scripts/EnvScripts/MeshVisionGen_Updated.cs
+++ Assets/01_Scripts/EnvScripts/MeshVisionGen_Updated.cs
@@ -36,7 +36,7 @@ public class MeshVisionGen_Updated : MonoBehaviour
     public Dictionary<Transform, SkinnedMeshRenderer[]> actorRenderers = new Dictionary<Transform, SkinnedMeshRenderer[]>();
     public Dictionary<Transform, MeshRenderer[]> accRenderers = new Dictionary<Transform, MeshRenderer[]>();
 
-    public bool isCashed = false;
+    public bool isDataSaved = false;
 
     IEnumerator Start()
     {
@@ -68,7 +68,7 @@ public class MeshVisionGen_Updated : MonoBehaviour
             accRenderers.Add(aiNum[i].transform, aiNum[i].GetComponentsInChildren<MeshRenderer>());
         }
 
-        isCashed = true;
+        isDataSaved = true;
         
         yield return new WaitForSeconds(1f);
 
