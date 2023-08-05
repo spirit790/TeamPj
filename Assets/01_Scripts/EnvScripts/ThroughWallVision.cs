9964a6d8716d50f33c8610dd14ae9e53b40ff998
geneleeverse <127931593+geneleeverse@users.noreply.github.com>
2023-08-06 02:58:00 +0900

    e

--- Assets/01_Scripts/EnvScripts/ThroughWallVision.cs
+++ Assets/01_Scripts/EnvScripts/ThroughWallVision.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using DG.Tweening;
 using System;
+using UnityEngine.TextCore.Text;
+using Unity.Mathematics;
 
 [System.Serializable]
 public class ThroughWallVision : MonoBehaviour
 {
-    private List<Transform> visibleWalls = new List<Transform>();
+    private List<Transform> transWalls = new List<Transform>();
 
     private Transform camTr;
-    private Transform characterTr;
+    public Transform characterTr;
     private int detectingLayer = 1 << 9 | 1 << 10;
     public float wallFreq = 0.1f;
 
@@ -29,13 +31,13 @@ public class ThroughWallVision : MonoBehaviour
         DOTween.Init();
         camTr = Camera.main.transform;
 
-        yield return new WaitUntil(()=> GameManager.Instance.isLoaded);
+        //yield return new WaitUntil(()=> GameManager.Instance.isLoaded);
 
         wallNum = GameObject.FindGameObjectsWithTag("WALL");
 
         for (int i = 0; i < wallNum.Length; i++)
         {
-            wallRenderers.Add(wallNum[i].transform, wallNum[i].GetComponents<Renderer>());
+            wallRenderers.Add(wallNum[i].transform, wallNum[i].GetComponentsInChildren<Renderer>());
             wallNum[i].layer = 9;
         }
 
@@ -58,28 +60,31 @@ public class ThroughWallVision : MonoBehaviour
             {
                 if (hits[i].transform.gameObject.layer == 9)
                 {
+                    //Make matarial transparent
                     WallAlphaChange(hits[i].transform, 0.5f);
-                    visibleWalls.Add(hits[i].transform);
+                    transWalls.Add(hits[i].transform);
                     hits[i].transform.gameObject.layer = 10;
                 }
                 else if (hits[i].transform.gameObject.layer == 10 && hits.Length == 1)
                 {
-                    for (int j = 0; j < visibleWalls.Count - 1; j++)
+                    //when only 1 wall remains
+                    for (int j = 0; j < transWalls.Count - 1; j++)
                     {
-                        WallAlphaChange(visibleWalls[j].transform, 1.0f);
-                        visibleWalls[j].transform.gameObject.layer = 9;
-                        visibleWalls.Remove(visibleWalls[j]);
+                        WallAlphaChange(transWalls[j].transform, 1.0f);
+                        transWalls[j].transform.gameObject.layer = 9;
+                        transWalls.Remove(transWalls[j]);
                     }
                 }
             }
 
+            //Make matarial opaque
             if (hits.Length == 0)
             {
-                for (int i = 0; i < visibleWalls.Count; i++)
+                for (int i = 0; i < transWalls.Count; i++)
                 {
-                    WallAlphaChange(visibleWalls[i].transform, 1.0f);
-                    visibleWalls[i].transform.gameObject.layer = 9;
-                    visibleWalls.Remove(visibleWalls[i]);
+                    WallAlphaChange(transWalls[i].transform, 1.0f);
+                    transWalls[i].transform.gameObject.layer = 9;
+                    transWalls.Remove(transWalls[i]);
                 }
             }
 
@@ -89,12 +94,14 @@ public class ThroughWallVision : MonoBehaviour
 
     private void WallAlphaChange(Transform wall, float alpha)
     {
-        if (wallRenderers[wall][0] == null) return;
+        if (wallRenderers[wall] == null) return;
 
         // Material의 Alpha를 바꾼다.
-        for (int i = 0; i < wallRenderers[wall].Length; i++)
+        for (int i = 0; i < wallRenderers[wall].Length-1; i++)
         {
-            ChangeMatAlpha(wallRenderers[wall][i], alpha);
+            //ChangeMatAlpha(wallRenderers[wall][i], alpha);
+            wallRenderers[wall][i].material.DOFade(alpha, wallFadeTime);
+
         }
     }
 
